754d8f19ac2cf4bce000a61cd6845b0cf8ffd918
ULADIEMER <uladiemer@outlook.com>
2015-01-17 19:04:53 +0300

    Initial project

---
+++ Solar System/Solar System/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Solar_System.MvcApplication" Language="C#" %>
---
+++ Solar System/Solar System/Scripts/zoom.js
@@ -0,0 +1,53 @@
+﻿var delta,
+    isCall = false,
+    zoomStep = 0.05;
+
+if (!isCall) {
+    var zoom = 1;
+    isCall = true;
+}
+
+function addHandler(object, event, handler) {
+    if (object.addEventListener) {
+        object.addEventListener(event, handler, false);
+    } else if (object.attachEvent) {
+        object.attachEvent('on' + event, handler);
+    } else alert("Обработчик не поддерживается");
+}
+
+addHandler(window, 'DOMMouseScroll', wheel);
+addHandler(window, 'mousewheel', wheel);
+addHandler(document, 'mousewheel', wheel);
+
+function wheel(event) {
+    event = event || window.event;
+
+    if (event.wheelDelta) {
+        delta = event.wheelDelta / 120;
+
+        if (window.opera) {
+            delta = -delta;
+        }
+    } else if (event.detail) {
+        delta = -event.detail / 3;
+    }
+
+    if (event.preventDefault) {
+        event.preventDefault();
+    } else {
+        event.returnValue = false;
+    }
+
+    if (delta > 0) {
+        zoom += zoomStep;
+        document.body.style.MozTransform = zoom;
+        document.body.style.OTransform = zoom;
+        document.body.style.zoom = zoom;
+    } else {
+        zoom -= zoomStep;
+        document.body.style.MozTransform = zoom;
+        document.body.style.OTransform = zoom;
+        document.body.style.zoom = zoom;
+    }
+}
+
