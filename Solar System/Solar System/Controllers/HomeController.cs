abc3e1cb328bb90cb42f9b9cfaf34b3b90f55bc6
ULADIEMER <uladiemer@outlook.com>
2015-01-18 13:32:23 +0300

    Repair configs

--- Solar System/Solar System/Controllers/HomeController.cs
+++ Solar System/Solar System/Controllers/HomeController.cs
@@ -1,12 +1,21 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using DAL;
 
 namespace Solar_System.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IDataManager _dm;
+
+        public HomeController(IDataManager dataManager)
+        {
+            _dm = dataManager;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            return View(_dm.SpaceObjects.GetAll().ToList());
         }
     }
 }
--- Solar System/Solar System/Global.asax.cs
+++ Solar System/Solar System/Global.asax.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Solar_System.DependencyResolver;
 
 namespace Solar_System
 {
@@ -16,6 +12,7 @@ namespace Solar_System
     {
         protected void Application_Start()
         {
+            DependencyResolverConfig.Register();
             AreaRegistration.RegisterAllAreas();
 
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
