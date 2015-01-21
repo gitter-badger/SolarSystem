bc6431702d9346327ab4c719ab74d10c7e5be782
ULADIEMER <uladiemer@outlook.com>
2015-01-21 10:24:11 +0300

    Add automapper config file

---
+++ Solar System/Solar System/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Model.Models;
+using Solar_System.Models;
+
+namespace Solar_System
+{
+    public class AutoMapperConfig
+    {
+        public static void RegisterAutoMapperRules()
+        {
+            Mapper.CreateMap<SpaceObject, SpaceObjectViewModel>();
+            Mapper.CreateMap<SpaceObjectViewModel, SpaceObject>();
+        }
+    }
+}
