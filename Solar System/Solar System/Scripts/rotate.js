b4717f1a2aab22c23c37e6cdc8b4075edd832872
ULADIEMER <uladiemer@outlook.com>
2015-02-02 16:26:07 +0300

    Update ellipse function

--- Solar System/Solar System/Scripts/rotate.js
+++ Solar System/Solar System/Scripts/rotate.js
@@ -7,14 +7,24 @@
     }
 
     function rotate(spaceObject) {
-        var f = 0,
-            s = 2 * Math.PI / 180;
+        var f = Math.floor((Math.random() * 360) + 0),
+            s = 0.1 * Math.PI / 180,
+            interval = 0.1 * $(spaceObject).attr("data-period") / 100,
+            isRetrograde = $(spaceObject).attr("data-is-retro"),
+            semiMinorAxis = $(spaceObject).attr("data-semi-minor"),
+            semiMajorAxis = $(spaceObject).attr("data-semi-major");
 
-        setInterval(function() {
-            f += s;
-            spaceObject.style.left = 235 + 149 * Math.sin(f) + 'px';
-            spaceObject.style.left = 235 + 149 * Math.cos(f) + 'px';
-        }, 1000);
+        setInterval(function () {
+
+            if (isRetrograde == true) {
+                f -= s;
+            } else {
+                f += s;
+            }
+
+            spaceObject.style.left = semiMajorAxis * Math.sin(f) + 'px';
+            spaceObject.style.top = semiMinorAxis * Math.cos(f) + 'px';
+        }, interval);
     }
 });
 
